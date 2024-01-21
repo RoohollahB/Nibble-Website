779ee35a0d3fbf46fedb83105a9d282b05b31dfd
Mohammad Amin <86496903+MohammadAminCodes@users.noreply.github.com>
2024-01-21 04:24:02 +0330

    previous version
    We created these files, but the responsive feature didn't work properly, so we left them out and didn't use them.
    sign up - login - forget password - pop up

---
+++ templates/previous version - some pages/login/login.js
@@ -0,0 +1,29 @@
+window.addEventListener('load', function(){
+    
+    var elements = document.querySelectorAll("body *");
+    var fontHash = {};
+ 
+    for(var e=0;e<elements.length;e++){
+        var elementStyles = window.getComputedStyle(elements[e],null);
+        var df = elementStyles['font-family'];
+ 
+        df = df.split('-').join(' ')
+            .split('+').join(' ')
+            .split(' ').join('')
+            .toLowerCase();
+ 
+        for(var gf = 0; gf<gfl.length; gf++){
+            if(df.indexOf(gfl[gf].split(' ').join('').toLowerCase())>-1){
+		        var fontName = gfl[gf];
+                if(!fontHash[fontName]) _agfh(gfl[gf]+':400,700,800');
+                fontHash[fontName] = 1;
+            }
+        }
+    }
+ 
+    function _agfh(n){
+       var gfli = "<link rel='stylesheet' href='https://fonts.googleapis.com/css?family="+n+"'>";
+	   document.head.innerHTML = document.head.innerHTML + gfli;
+    }
+    
+},false);
